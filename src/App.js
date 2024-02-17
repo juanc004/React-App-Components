0c55869c25b04644700aa2f60379ffe8c5f29891
juanc004 <124308226+juanc004@users.noreply.github.com>
2024-02-17 07:46:38 -0600

    Created tree-view sidebar component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
     <div className="App">
-      {/* <Accordion />
+      <Accordion />
       <RandomColor />
-      <StarRating noOfStars={10} /> */}
+      <StarRating noOfStars={10} />
 
-      {/* <ImageSlider
+      <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      /> */}
+      />
       <LoadMoreData />
+
+      <TreeView menus={menus} />
     </div>
   );
 }
